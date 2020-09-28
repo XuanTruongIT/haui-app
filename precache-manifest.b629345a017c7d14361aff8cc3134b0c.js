b0e3fea48242756dfa08c706c92098d90a62766c
XuanSon99 <xuansonn12499@gmail.com>
2020-09-28 10:45:19 +0700

    Updates

---
+++ precache-manifest.b629345a017c7d14361aff8cc3134b0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceaf90dfa0784c5ec6e3befcd1728bd1",
+    "url": "/haui-app/index.html"
+  },
+  {
+    "revision": "53f5b7ef17e6b43eee9d",
+    "url": "/haui-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7379a216e3a7f5d9b378",
+    "url": "/haui-app/static/css/main.6f9b839d.chunk.css"
+  },
+  {
+    "revision": "53f5b7ef17e6b43eee9d",
+    "url": "/haui-app/static/js/2.808ab87c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/haui-app/static/js/2.808ab87c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7379a216e3a7f5d9b378",
+    "url": "/haui-app/static/js/main.e3b05b5a.chunk.js"
+  },
+  {
+    "revision": "a5b8dfa46a09c665e93d",
+    "url": "/haui-app/static/js/runtime-main.22183a11.js"
+  }
+]);
